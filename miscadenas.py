2267b06e9f3d7905fc0fbf63e4cf523feed8e6ad
ValentinLoya <a.22308051281244@cbtis128.edu.mx>
2024-09-06 09:32:56 -0600

    Add files via upload
    Subi 2 archivos

---
+++ miscadenas.py
@@ -0,0 +1,22 @@
+"""Valentin Loya
+Trabajando con cadenas"""
+
+nombre = "Valentin"
+print(nombre[0])
+print(nombre)
+
+MisNovias = ["Agripina","Anastacia","Maria"]
+for chicas in MisNovias:
+    print(chicas)
+    
+    print("otro ejemplo")
+    b = "Valentin Loya"
+    print(b[0:13])
+    print(" a mayusculas")
+    a = "valentin loya"
+print(a.upper())
+print("Concatenar")
+a = "Maria"
+b = "Perez"
+c = a +" "+ b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,16 @@
+import math
+edad=17  # 17 se guarda en edad
+
+nombre="Valentin Loya"
+print("Tu edad es ",edad)
+print("Tu nombre es" ,nombre)
+print("-- tipo de variables ---")
+x = str(7)    # x will be '3'
+y = int(3.1416)    # y will be 3
+z = float(7)
+print(x)
+print(y)
+print(z)
+print("--funciones matematicas----")
+x = math.sqrt(64)
+print(x)
